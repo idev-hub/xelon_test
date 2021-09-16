342ee44f089f0bbf5a4dae022064e55c58e10ae4
idev <idev.hub@ya.ru>
2021-09-16 05:28:38 +0500

    Папочка dist

---
+++ dist/main.f2a34db4da4d9482c439.js
@@ -0,0 +1,309 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./assets/js/test.js":
+/*!***************************!*\
+  !*** ./assets/js/test.js ***!
+  \***************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ test; }
+/* harmony export */ });
+function test() {
+  console.log("test");
+}
+
+/***/ }),
+
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _svg_logo_svg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @svg/logo.svg */ "./assets/images/svg/logo.svg");
+/* harmony import */ var _svg_burger_svg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @svg/burger.svg */ "./assets/images/svg/burger.svg");
+/* harmony import */ var _svg_close_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @svg/close.svg */ "./assets/images/svg/close.svg");
+/* harmony import */ var _assets_scss_main_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @assets/scss/main.scss */ "./assets/scss/main.scss");
+/* harmony import */ var _assets_js_test__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @assets/js/test */ "./assets/js/test.js");
+
+
+
+
+
+(0,_assets_js_test__WEBPACK_IMPORTED_MODULE_4__["default"])();
+
+/***/ }),
+
+/***/ "./assets/scss/main.scss":
+/*!*******************************!*\
+  !*** ./assets/scss/main.scss ***!
+  \*******************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./assets/images/svg/burger.svg":
+/*!**************************************!*\
+  !*** ./assets/images/svg/burger.svg ***!
+  \**************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../node_modules/svg-baker-runtime/browser-symbol.js */ "../node_modules/svg-baker-runtime/browser-symbol.js");
+/* harmony import */ var _node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../node_modules/svg-sprite-loader/runtime/browser-sprite.build.js */ "../node_modules/svg-sprite-loader/runtime/browser-sprite.build.js");
+/* harmony import */ var _node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1__);
+
+
+var symbol = new (_node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0___default())({
+  "id": "burger",
+  "use": "burger-usage",
+  "viewBox": "0 0 50 50",
+  "content": "<symbol xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 50 50\" id=\"burger\"><path d=\"M47.917 12.5H2.084v2h45.833zM47.917 21.5H25v2h22.917zM47.917 30.5H2.084v2h45.833z\" /></symbol>"
+});
+var result = _node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1___default().add(symbol);
+/* harmony default export */ __webpack_exports__["default"] = (symbol);
+
+/***/ }),
+
+/***/ "./assets/images/svg/close.svg":
+/*!*************************************!*\
+  !*** ./assets/images/svg/close.svg ***!
+  \*************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../node_modules/svg-baker-runtime/browser-symbol.js */ "../node_modules/svg-baker-runtime/browser-symbol.js");
+/* harmony import */ var _node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../node_modules/svg-sprite-loader/runtime/browser-sprite.build.js */ "../node_modules/svg-sprite-loader/runtime/browser-sprite.build.js");
+/* harmony import */ var _node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1__);
+
+
+var symbol = new (_node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0___default())({
+  "id": "close",
+  "use": "close-usage",
+  "viewBox": "0 0 50 50",
+  "content": "<symbol xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 50 50\" id=\"close\"><path d=\"m9.25 6.944 33.806 33.807-1.414 1.414L7.836 8.359z\" /><path d=\"M6.944 40.751 40.751 6.945l1.414 1.414L8.359 42.165z\" /></symbol>"
+});
+var result = _node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1___default().add(symbol);
+/* harmony default export */ __webpack_exports__["default"] = (symbol);
+
+/***/ }),
+
+/***/ "./assets/images/svg/logo.svg":
+/*!************************************!*\
+  !*** ./assets/images/svg/logo.svg ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../node_modules/svg-baker-runtime/browser-symbol.js */ "../node_modules/svg-baker-runtime/browser-symbol.js");
+/* harmony import */ var _node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../node_modules/svg-sprite-loader/runtime/browser-sprite.build.js */ "../node_modules/svg-sprite-loader/runtime/browser-sprite.build.js");
+/* harmony import */ var _node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1__);
+
+
+var symbol = new (_node_modules_svg_baker_runtime_browser_symbol_js__WEBPACK_IMPORTED_MODULE_0___default())({
+  "id": "logo",
+  "use": "logo-usage",
+  "viewBox": "0 0 500 150",
+  "content": "<symbol xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 150\" id=\"logo\"><path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"m76.189.005.01-.005v.01c2.974 1.639 58.231 32.017 58.231 32.017.28.103 17.951 7.473 17.951 19.346v2.178c0 8.955-10.06 15.318-14.856 17.846-3.175-1.705-28.877-15.767-46.09-25.185l-.017-.01-.009-.003c-7.716-4.222-13.72-7.508-15.22-8.325-.783.43-55.564 30.54-62.035 33.857C8.896 74.478 2.964 78.605 0 84.165V51.358C0 39.5 17.67 32.13 17.865 32.069c0 0 55.342-30.429 58.314-32.058V0l.01.005Zm-.01 149.984v.008l.007-.005.012.008v-.015c2.975-1.628 58.324-32.047 58.324-32.047.187-.07 17.859-7.442 17.859-19.299V65.832c-2.972 5.554-8.895 9.687-14.153 12.416-6.471 3.334-61.252 33.443-62.04 33.876-4.83-2.653-56.734-31.052-61.331-33.52C10.064 81.135 0 87.488 0 96.443v2.184c0 11.873 17.67 19.231 17.954 19.345 0 0 55.254 30.371 58.225 32.018Z\" /><path d=\"M342.54 23.657c-2.234 0-4.044 1.783-4.044 3.982v78.21c0 2.199 1.81 3.982 4.044 3.982h6.396c2.234 0 4.046-1.783 4.046-3.982v-78.21c0-2.2-1.812-3.982-4.046-3.982h-6.396Z\" /><path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M297.071 44.203c-9.683 0-17.662 3.422-23.209 9.48-5.534 6.044-8.559 14.612-8.559 24.762 0 9.544 3.096 17.83 8.751 23.737 5.661 5.916 13.805 9.361 23.714 9.361 12.241 0 23.592-6.508 28.546-18.045l.491-1.144-13.994-4.456-.434.903c-2.922 6.074-8.288 9.166-15.306 9.166-5.065 0-8.917-1.617-11.623-4.45-2.513-2.632-4.134-6.414-4.723-11.195h46.546l.06-1.076c.589-10.677-1.671-19.934-6.787-26.549-5.143-6.651-13.069-10.494-23.473-10.494Zm.013 12.892c5.173-.055 8.687 1.309 11.059 3.708 2.188 2.211 3.54 5.438 4.193 9.655h-31.161c.817-3.978 2.379-7.197 4.721-9.467 2.572-2.492 6.2-3.95 11.162-3.896h.026ZM395.796 44.206c-9.511 0-17.482 3.393-23.07 9.355-5.579 5.953-8.698 14.378-8.698 24.314 0 9.76 3.03 18.182 8.562 24.179 5.543 6.007 13.516 9.49 23.206 9.49 9.627 0 17.629-3.422 23.216-9.399 5.579-5.968 8.67-14.393 8.67-24.27 0-9.76-3.03-18.184-8.579-24.18-5.557-6.009-13.56-9.49-23.307-9.49Zm-16.584 33.669c0-6.136 1.363-11.164 4.066-14.633 2.665-3.42 6.742-5.461 12.518-5.461 5.672 0 9.782 1.963 12.502 5.347 2.754 3.425 4.201 8.448 4.201 14.747 0 6.126-1.417 11.154-4.163 14.625-2.712 3.43-6.821 5.469-12.54 5.469-5.549 0-9.632-1.96-12.356-5.348-2.756-3.427-4.228-8.452-4.228-14.746Z\" /><path d=\"M487.495 50.308c-5.985-5.17-13.286-6.457-20.205-5.989-5.856.354-12.304 2.47-16.626 7.177v-1.599c0-2.199-1.812-3.982-4.046-3.982h-5.006c-2.233 0-4.044 1.783-4.044 3.982v55.951c0 2.199 1.811 3.981 4.044 3.981h6.397c2.234 0 4.045-1.782 4.045-3.981V81.64c0-4.5.008-9.03.783-12.296l.012-.056c.659-3.573 2.291-6.6 4.677-8.44l.02-.016c2.321-1.869 5.199-2.834 8.482-3.053 4.189-.105 7.684.986 10.065 3.127l.028.024c2.198 1.855 3.555 4.44 4.355 7.589.81 3.16 1.038 6.815 1.038 10.723v26.606c0 2.199 1.815 3.981 4.048 3.981h6.514c2.234 0 4.043-1.782 4.043-3.981V78.793c.114-10.213-.529-21.48-8.624-28.485ZM207.7 45.917c-3.323 0-5.228 3.726-3.249 6.354l19.031 25.261-19.505 25.949c-1.976 2.628-.07 6.351 3.252 6.351h7.175c1.294 0 2.51-.609 3.271-1.639l14.315-19.364 14.203 19.353a4.07 4.07 0 0 0 3.28 1.65h7.154c3.324 0 5.231-3.732 3.244-6.358l-19.595-25.943 19.028-25.26c1.981-2.628.075-6.354-3.248-6.354h-7.158a4.067 4.067 0 0 0-3.286 1.657l-13.625 18.658-13.847-18.68a4.068 4.068 0 0 0-3.269-1.635H207.7Z\" /></symbol>"
+});
+var result = _node_modules_svg_sprite_loader_runtime_browser_sprite_build_js__WEBPACK_IMPORTED_MODULE_1___default().add(symbol);
+/* harmony default export */ __webpack_exports__["default"] = (symbol);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	!function() {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = function(result, chunkIds, fn, priority) {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var chunkIds = deferred[i][0];
+/******/ 				var fn = deferred[i][1];
+/******/ 				var priority = deferred[i][2];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every(function(key) { return __webpack_require__.O[key](chunkIds[j]); })) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	!function() {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	!function() {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"main": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = function(chunkId) { return installedChunks[chunkId] === 0; };
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = function(parentChunkLoadingFunction, data) {
+/******/ 			var chunkIds = data[0];
+/******/ 			var moreModules = data[1];
+/******/ 			var runtime = data[2];
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some(function(id) { return installedChunks[id] !== 0; })) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkIds[i]] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["vendors-node_modules_babel_polyfill_lib_index_js-node_modules_svg-baker-runtime_browser-symbo-248ad9"], function() { return __webpack_require__("../node_modules/@babel/polyfill/lib/index.js"); })
+/******/ 	__webpack_require__.O(undefined, ["vendors-node_modules_babel_polyfill_lib_index_js-node_modules_svg-baker-runtime_browser-symbo-248ad9"], function() { return __webpack_require__("../node_modules/webp-in-css/polyfill.js"); })
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["vendors-node_modules_babel_polyfill_lib_index_js-node_modules_svg-baker-runtime_browser-symbo-248ad9"], function() { return __webpack_require__("./main.js"); })
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=main.f2a34db4da4d9482c439.js.map
